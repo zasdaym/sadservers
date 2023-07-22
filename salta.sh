514bf578a7350b8560e90c44c3f080e83dc94f55
Zasda Yusuf Mikail <zasdaym@gmail.com>
2023-07-22 22:24:05 +0700

    Add salta.sh

---
+++ salta.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo netstat -tulpn | grep ':8888'
+sudo systemctl stop nginx
+
+cd /home/admin/app
+sed -i '/^EXPOSE/d' Dockerfile
+sed -i 's/serve.js/server.js/' Dockerfile
+sudo docker build -t app .
+sudo docker run --name app -d -p 8888:8888 app
+curl lcoalhost:8888
