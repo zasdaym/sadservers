02870a766894f39341662ed847762ca01badfd98
Zasda Yusuf Mikail <zasdaym@gmail.com>
2023-07-22 22:24:27 +0700

    Update salta.sh

--- salta.sh
+++ salta.sh
@@ -5,7 +5,7 @@ sudo systemctl stop nginx
 
 cd /home/admin/app
 sed -i '/^EXPOSE/d' Dockerfile
-sed -i 's/serve.js/server.js/' Dockerfile
+sed -i 's/serve.js/server.js/g' Dockerfile
 sudo docker build -t app .
 sudo docker run --name app -d -p 8888:8888 app
 curl lcoalhost:8888
