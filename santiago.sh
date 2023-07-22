922a00ef2a435eab23aa75545af395a199b04823
Zasda Yusuf Mikail <zasdaym@gmail.com>
2023-07-22 19:29:26 +0700

    Improve santiago.sh

--- santiago.sh
+++ santiago.sh
@@ -6,10 +6,11 @@ matches=$(grep -c -r Alice /home/admin/*.txt)
 for match in $matches; do
     file=$(echo $match | cut -d ':' -f 1)
     count=$(echo $match | cut -d ':' -f 2)
-    if [[ $count == "1" ]]; then
-        let line=$(grep -n Alice $file | cut -d ':' -f 1)+1
-        second=$(head -n $line $file | tail -n 1 | tr -d -c '0-9')
-        echo "${first}${second}" >/home/admin/solution
-        break
+    if [[ $count != "1" ]]; then
+        continue
     fi
+    let line=$(grep -n Alice $file | cut -d ':' -f 1)+1
+    second=$(head -n $line $file | tail -n 1 | tr -d -c '0-9')
+    echo "${first}${second}" >/home/admin/solution
+    break
 done
