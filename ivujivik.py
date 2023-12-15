4ae71ac2caf364d79c07f74284eb0446200dfe6d
Zasda Yusuf Mikail <zasdaym@gmail.com>
2023-12-15 23:42:59 +0700

    Add ivujivik

---
+++ ivujivik.py
@@ -0,0 +1,21 @@
+import csv
+
+result = ""
+
+with open('/home/admin/table_tableau11.csv', 'r') as file:
+    max_rejected_ballots = 0
+
+    reader = csv.reader(file) 
+    csv.next(reader)
+    for row in reader:
+        district_name, population, rejected_ballots = row[1], int(row[3]), int(row[8])
+        print(f"{district_name} | {population} | {rejected_ballots}")
+        if population >= 100000:
+            print(f"Skipping {district_name} because the population is {population}")
+            continue
+        if rejected_ballots > max_rejected_ballots:
+            max_rejected_ballots = rejected_ballots
+            result = district_name
+
+with open('/home/admin/mysolution', 'w') as file:
+    file.writelines(result)
